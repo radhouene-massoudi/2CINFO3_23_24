c435718ceafd87d69e33d668f84ca4562c6e5868
radhouene-massoudi <massoudiradhouene@gmail.com>
2024-01-16 20:11:34 +0100

    seance 16/01/24

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -8,10 +8,25 @@ import { Component } from '@angular/core';
 export class FirstComponent {
 klass='2cinfo3 sjfsjfsf';
 color="yellow"
+search="";
+Products=[
+  {name:"hp",price:1500,like:0,dislike:5,quantity:5},
+  {name:"lennovo",price:500,like:0,dislike:5,quantity:0},
+  {name:"asus",price:450,like:0,dislike:5,quantity:5},
+]
 showMsg(){
   alert('bonjour');
 }
 changeColor(){
  this.color='green' 
+}
+IncreaseLike(pro:any){
+  let index=this.Products.indexOf(pro)
+//console.log(pro)
+
+this.Products[index].like++
+}
+dislike(index:any){
+this.Products[index].dislike--
 }
 }
